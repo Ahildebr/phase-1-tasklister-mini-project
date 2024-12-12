1683a09ee2b5a7ab8962c67b553389db9b18c82e
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2024-12-11 16:27:12 -0800

    Makes changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ const taskBox = document.getElementById("create-task-form")
+ const taskList = document.getElementById("tasks")
+ 
+ taskBox.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const taskInput = document.getElementById("new-task-description").value 
+    const priority = document.getElementById("priority").value
+    const li = document.createElement("li")
+    li.textContent = taskInput // 1.Creates list 2.Adds the Value of "new-task-description"
+    
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "X" //Makes the x button
+    deleteButton.addEventListener("click", () => li.remove())
+    
+
+    
+  //appends  
+  li.appendChild(deleteButton) //adds delete button
+  taskList.appendChild(li) //adds tasks to new tasklist
+    
+  
+  taskBox.reset(); // Clears my "Task Description"
+  })
+  
 });
